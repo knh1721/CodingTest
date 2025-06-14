a548e1799135d15ff6d8cf9ddfb3144953033b38
knh1721 <rnjsskgus2@gmail.com>
2025-06-15 00:01:57 +0900

    [level 0] Title: 배열 만들기 4, Time: 32.90 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            if(stk.isEmpty()){
+                stk.add(arr[i]);
+            }else if(!stk.isEmpty() && stk.get(stk.size() - 1) < arr[i]){
+                stk.add(arr[i]);
+            }else if(!stk.isEmpty() && stk.get(stk.size() - 1) >= arr[i]){
+                stk.remove(stk.get(stk.size() - 1));
+                i--;
+            }
+        }
+        return stk.stream().mapToInt(i->i).toArray();
+    }
+}
