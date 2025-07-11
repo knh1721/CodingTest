f43cb5f08d5748a6c93010a1e651f10d3fac3a81
knh1721 <rnjsskgus2@gmail.com>
2025-07-11 23:02:25 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer = 12000*n + 2000*k;
+        
+        if(n/10 > 0){
+            answer -= n/10 * 2000;
+        }
+        return answer;
+    }
+}
