12e064487c3fae2cbd68e51a4af9f641980c2909
knh1721 <rnjsskgus2@gmail.com>
2025-07-24 19:54:38 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int cnt = 0;
+        for(int i : array){
+            if(i > height){
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+}
